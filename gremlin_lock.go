a92df292437752e4c0f2c1b701db816cea887e42
Evan Marcey <emarcey@cbinsights.com>
2019-01-23 16:19:19 -0500

    Better description of Locks

--- gremlin_lock.go
+++ gremlin_lock.go
@@ -5,17 +5,28 @@ import (
 	"sync"
 )
 
+// LockClient_i is an interface for concurrency locking mechanisms
+// With a set of keys which can be locked and unlocked independently of each other
+// LockClient_i requires a LockKey method, which accepts a string key and returns the corresponding Lock for that key
+
 type LockClient_i interface {
 	LockKey(key string) (Lock_i, error)
 }
 
+// Lock_i is an instance of a Lock in the set of LockClient_i locks
+// It requires 3 methods for implementation
+//    - Lock: which attempts to establish a lock on its key
+//    - Unlock: which attempts to remove a lock on its key
+//    - Destroy: which deletes the Lock_i's key from its parent client
 type Lock_i interface {
 	Lock() error
 	Unlock() error
 	Destroy() error
 }
 
-// Creates a lock using a sync map
+// The LocalLock is an implementation of the LockClient_i/Lock_i structure
+// That is designed to be used in a non-distributed environment
+// It uses a sync Map of Mutexes which are locked and unlocked according to a specified key
 type LocalLockClient struct {
 	Keys *sync.Map
 }
